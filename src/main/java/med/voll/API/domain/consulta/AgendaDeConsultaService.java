2a919347ddc263d547a38d29b8f3c64878433b64
Tamerjms <tamerjms94@hotmail.com>
2024-07-05 19:38:40 -0400

    Aplicacion de principios Solid para verificar reglas de negocio

--- src/main/java/med/voll/API/domain/consulta/AgendaDeConsultaService.java
+++ src/main/java/med/voll/API/domain/consulta/AgendaDeConsultaService.java
@@ -1,5 +1,6 @@
 package med.voll.API.domain.consulta;
 
+import med.voll.API.domain.consulta.validaciones.ValidadorDeConsultas;
 import med.voll.API.domain.medico.Medico;
 import med.voll.API.domain.medico.MedicoRepository;
 import med.voll.API.domain.paciente.Paciente;
@@ -7,6 +8,8 @@ import med.voll.API.domain.paciente.PacienteRepository;
 import med.voll.API.infra.errores.ValidacionDeIntegridad;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AgendaDeConsultaService {
@@ -20,6 +23,9 @@ public class AgendaDeConsultaService {
     @Autowired
     private ConsultaRepository consultaRepository;
 
+    @Autowired
+    List<ValidadorDeConsultas> validadores;
+
     public void agendar(DatosAgendarConsulta datos){
 
         if(pacienteRepository.findById(datos.idPaciente()).isPresent()){
@@ -29,6 +35,9 @@ public class AgendaDeConsultaService {
         if(datos.idMedico() != null && medicoRepository.existsById(datos.idMedico())){
             throw new ValidacionDeIntegridad("Este id para el medico no fue encontrado");
         }
+
+
+        validadores.forEach(v->v.validar(datos));
 
         var paciente = pacienteRepository.findById(datos.idPaciente()).get();
 
---
+++ src/main/java/med/voll/API/domain/consulta/validaciones/ValidadorDeConsultas.java
@@ -0,0 +1,8 @@
+package med.voll.API.domain.consulta.validaciones;
+
+import med.voll.API.domain.consulta.DatosAgendarConsulta;
+
+public interface ValidadorDeConsultas {
+    public void validar(DatosAgendarConsulta datos);
+
+}
